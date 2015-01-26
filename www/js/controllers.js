58cb5c16068d1a7d095b2ac473a3c16a5a10abfd
monsoul <monsoul@msn.com>
2015-01-26 21:57:53 +0800

    add circle

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,92 +1,206 @@
-angular.module('starter.controllers', ['Simos.directive'])
+angular.module('starter.controllers', ['Simos.directive', 'ionic'])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http, $cookieStore, $rootScope) {
-  // Form data for the login modal
-  $scope.loginData = {};
-  var server='http://192.168.1.101:8000';
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, $http, $cookieStore, $rootScope, $ionicPopover, $ionicPopup, $timeout) {
+    // Form data for the login modal
+    $scope.loginData = {};
+    var server = 'http://192.168.1.101:8000';
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    //var template = 'I really understand this!';
+    var template = '<ion-popover-view><ion-content> Hello! </ion-content></ion-popover-view>';
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  $scope.logout=function(){
-    $cookieStore.remove('user');
-    $rootScope.user = $cookieStore.get('user') || {
-      username: '',
-      role: 'guest'
-    };
-  }
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    var postData = {
-      username: $scope.loginData.username + '^8^1',
-      password: $scope.loginData.password,
-      rememberme: true
-    };
-
-    $http({
-      method: 'POST',
-      url: server + '/login',
-      data: postData
-    }).then(function(data) {
-
-      $cookieStore.put('user', {
-        'username': $scope.loginData.username,
-        'role': 'student'
-      });
-      $rootScope.user = $cookieStore.get('user') || {
-        username: '',
-        role: 'guest'
-      };
-
-      $timeout(function() {
-        $scope.closeLogin();
-      }, 1000);
-
-    }, function(data) {
-      console.log('fail:' + data);
-
-      $cookieStore.remove('user');
-
+    $scope.popover = $ionicPopover.fromTemplate(template, {
+        scope: $scope,
     });
-  };
 
-  $scope.showManual=function(item){
-    $scope.cardEmailShow=false;
-    $scope.cardPhoneShow=false;
+    var closePopover = function() {
+        $scope.popover.hide();
+    };
 
-    switch(item){
-      case 'cardEmailShow':$scope.cardEmailShow=true;break;
-      case 'cardPhoneShow':$scope.cardPhoneShow=true;break;
+    $scope.openPopover = function($event) {
+        console.log($event);
+        $event.clientY=100;
+        $event.clientX=100;
+
+        $scope.popover.show($event);
+    };
+
+    
+
+    $scope.showPopup = function() {
+        console.log('test');
+        $scope.data = {}
+
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+            template: '<input type="password" ng-model="data.wifi">',
+            title: 'Enter Wi-Fi Password',
+            subTitle: 'Please use normal things',
+            scope: $scope,
+            buttons: [{
+                text: 'Cancel'
+            }, {
+                text: '<b>Save</b>',
+                type: 'button-positive',
+                onTap: function(e) {
+                    if (!$scope.data.wifi) {
+                        //don't allow the user to close unless he enters wifi password
+                        e.preventDefault();
+                    } else {
+                        return $scope.data.wifi;
+                    }
+                }
+            }]
+        });
+        myPopup.then(function(res) {
+            console.log('Tapped!', res);
+        });
+        $timeout(function() {
+            myPopup.close(); //close the popup after 3 seconds for some reason
+        }, 3000);
+    };
+    // A confirm dialog
+    $scope.showConfirm = function() {
+        var confirmPopup = $ionicPopup.confirm({
+            title: 'Consume Ice Cream',
+            template: 'Are you sure you want to eat this ice cream?'
+        });
+        confirmPopup.then(function(res) {
+            if (res) {
+                console.log('You are sure');
+            } else {
+                console.log('You are not sure');
+            }
+        });
+    };
+
+    // An alert dialog
+    $scope.showAlert = function() {
+        var alertPopup = $ionicPopup.alert({
+            title: 'Don\'t eat that!',
+            template: 'It might taste good'
+        });
+        alertPopup.then(function(res) {
+            console.log('Thank you for not eating my delicious ice cream cone');
+        });
+    };
+
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.popover.remove();
+    });
+    // Execute action on hide popover
+    $scope.$on('popover.hidden', function() {
+        // Execute action
+    });
+    // Execute action on remove popover
+    $scope.$on('popover.removed', function() {
+        // Execute action
+    });
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+        scope: $scope
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.myRange = 0;
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function() {
+        $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function() {
+        $scope.modal.show();
+    };
+
+
+
+    $scope.logout = function() {
+        $cookieStore.remove('user');
+        $rootScope.user = $cookieStore.get('user') || {
+            username: '',
+            role: 'guest'
+        };
     }
-  }
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function() {
+        var postData = {
+            username: $scope.loginData.username + '^8^1',
+            password: $scope.loginData.password,
+            rememberme: true
+        };
+
+        $http({
+            method: 'POST',
+            url: server + '/login',
+            data: postData
+        }).then(function(data) {
+
+            $cookieStore.put('user', {
+                'username': $scope.loginData.username,
+                'role': 'student'
+            });
+            $rootScope.user = $cookieStore.get('user') || {
+                username: '',
+                role: 'guest'
+            };
+
+            $timeout(function() {
+                $scope.closeLogin();
+            }, 1000);
+
+        }, function(data) {
+            console.log('fail:' + data);
+
+            $cookieStore.remove('user');
+
+        });
+    };
+
+    $scope.showManual = function(item) {
+        $scope.cardEmailShow = false;
+        $scope.cardPhoneShow = false;
+
+        switch (item) {
+            case 'cardEmailShow':
+                $scope.cardEmailShow = true;
+                break;
+            case 'cardPhoneShow':
+                $scope.cardPhoneShow = true;
+                break;
+        }
+    }
+
+    $scope.$watch('myRange',function(oldValue,newValue){
+
+    })
 
 })
 
 .controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+    $scope.playlists = [{
+        title: 'Reggae',
+        id: 1
+    }, {
+        title: 'Chill',
+        id: 2
+    }, {
+        title: 'Dubstep',
+        id: 3
+    }, {
+        title: 'Indie',
+        id: 4
+    }, {
+        title: 'Rap',
+        id: 5
+    }, {
+        title: 'Cowbell',
+        id: 6
+    }];
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('PlaylistCtrl', function($scope, $stateParams) {});
--- www/js/directives.js
+++ www/js/directives.js
@@ -10,6 +10,129 @@
                 console.log('directive');
             }
         }
-    ])
+    ]);
+
+    directive.directive('testRange',[])
+    directive.directive('testRange', ['$compile', 
+        function($compile) {
+            return function(scope, element, attr) {
+                var cust = document.querySelector('#js-test');
+                var initCust = new Powerange(cust, {
+                    hideRange: true,
+                    start: 0,
+                    max:3,
+                    min:-3,
+                    step:1
+                });
+            }
+        }
+    ]);
+
+    directive.directive('inputRange1', ['$compile', 
+        function($compile) {
+            return function(scope, element, attr) {
+                var cust = document.querySelector('#js-vertical1');
+                var initCust = new Powerange(cust, {
+                    hideRange: true,
+                    klass: 'power-ranger',
+                    start: 0,
+                    max:3,
+                    min:-3,
+                    step:1,
+                    vertical: true
+                });
+            }
+        }
+    ]);
+    
+    directive.directive('inputRange2', ['$compile', 
+        function($compile) {
+            return function(scope, element, attr) {
+                var cust = document.querySelector('#js-vertical2');
+                var initCust = new Powerange(cust, {
+                    hideRange: true,
+                    klass: 'power-ranger',
+                    start: 0,
+                    max:3,
+                    min:-3,
+                    step:1,
+                    vertical: true
+                });
+            }
+        }
+    ]);
+    directive.directive('inputRange3', ['$compile', 
+        function($compile) {
+            return function(scope, element, attr) {
+                var cust = document.querySelector('#js-vertical3');
+                var initCust = new Powerange(cust, {
+                    hideRange: true,
+                    klass: 'power-ranger',
+                    start: 0,
+                    max:3,
+                    min:-3,
+                    step:1,
+                    vertical: true
+                });
+            }
+        }
+    ]);
+
+    directive.directive('inputRange3', ['$compile', 
+        function($compile) {
+            return function(scope, element, attr) {
+                var cust = document.querySelector('#js-vertical3');
+                var initCust = new Powerange(cust, {
+                    hideRange: true,
+                    klass: 'power-ranger',
+                    start: 0,
+                    max:3,
+                    min:-3,
+                    step:1,
+                    vertical: true
+                });
+            }
+        }
+    ]);
+
+    directive.directive('circleBody', ['$compile',
+        function($compile) {
+            return function(scope, element, attr) {
+
+                jQuery(function($) {
+
+                    var cirecleLeft = ($('#myCircle')[0].offsetWidth - 300) / 2;
+                    var cirecleTop = ($('#myCircle')[0].offsetHeight - 300) / 2;
+
+                    $('#canvas').css({
+                        top: cirecleTop,
+                        left: cirecleLeft
+                    });
+                    $('#drag').css({
+                        top: cirecleTop - 10,
+                        left: cirecleLeft - 10 + 150
+                    });
+                    $('#drag').drag('start', function(ev, dd) {
+                        if (!$.data(this, 'circle')) {
+                            $.data(this, 'circle', {
+                                radius: 150,
+                                centerX: cirecleLeft + 150 - 10,
+                                centerY: cirecleTop + 150 - 10
+                            });
+                        }
+                    })
+                    $('#drag').drag(function(ev, dd) {
+                        var data = $.data(this, 'circle'),
+                            angle = Math.atan2(ev.pageX - data.centerX, ev.pageY - data.centerY);
+                        $(this).css({
+                            top: data.centerY + Math.cos(angle) * data.radius,
+                            left: data.centerX + Math.sin(angle) * data.radius
+                        });
+                    });
+                });
+            }
+        }
+    ]);
+
 
 })();
